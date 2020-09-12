6cd22becfd4371c39100947e0faadb1f0c0f2c11
ganiaaldi <mrgad98@gmail.com>
2020-09-12 20:10:47 +0700

    change img to sky mode

--- parralax.js
+++ parralax.js
@@ -2,14 +2,9 @@ $(window).scroll(function(){
    var winScroll = $(this).scrollTop();
    console.log(winScroll);
    
-   $('.bg').css({
-      'transform': 'translate(0,'+ winScroll / 6 +'%)'
-   })
+
    $('.bg2').css({
-      'transform': 'translate(0,'+ winScroll / 10 +'%)'
-   })
-   $('.bg3').css({
-      'transform': 'translate(0,'+ winScroll / 16 +'%)'
+      'transform': 'translate(0,'+ winScroll / 6 +'%)'
    })
    $('.bg4').css({
       'transform': 'translate(0,'+ winScroll / 24 +'%)'
